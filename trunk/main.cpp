fb0be9f50456d3bb4f109b75e8675e01d604ed25
qverdi@hotmail.com <qverdi@hotmail.com@fdc640ea-bcce-27a7-7e40-59ee6ab0b469>
2013-01-04 21:26:01 +0000

    Car hypothesis validation using X symmetry and horizontal edge features.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -53,7 +53,27 @@ struct Status {
 	int lost;
 };
 
+struct Vehicle {
+	CvPoint bmin, bmax;
+	int symmetryX;
+	bool valid;
+	unsigned int lastUpdate;
+};
+
+struct VehicleSample {
+	CvPoint center;
+	float radi;
+	unsigned int frameDetected;
+	int vehicleIndex;
+};
+
+#define GREEN CV_RGB(0,255,0)
+#define RED CV_RGB(255,0,0)
+#define PURPLE CV_RGB(255,0,255)
+
 Status laneR, laneL;
+std::vector<Vehicle> vehicles;
+std::vector<VehicleSample> samples;
 
 enum{
     SCAN_STEP = 5,			 // in pixels
@@ -62,12 +82,19 @@ enum{
     BORDERX = 10,			 // px, skip this much from left & right borders
 	MAX_RESPONSE_DIST = 5,	 // px
 	
-	CANNY_MIN_TRESHOLD = 1,
-	CANNY_MAX_TRESHOLD = 100,
+	CANNY_MIN_TRESHOLD = 1,	  // edge detector minimum hysteresis threshold
+	CANNY_MAX_TRESHOLD = 100, // edge detector maximum hysteresis threshold
 
 	HOUGH_TRESHOLD = 50,
-	HOUGH_MIN_LINE_LENGTH = 10,
+	HOUGH_MIN_LINE_LENGTH = 10,	// join line with smaller than this gaps
 	HOUGH_MAX_LINE_GAP = 100,
+
+	CAR_DETECT_LINES = 4,   // minimum lines for a region to pass validation as a 'CAR'
+	CAR_H_LINE_LENGTH = 10,  // minimum horizontal line length from car body in px
+
+	MAX_VEHICLE_SAMPLES = 30,      // max vehicle detection sampling history
+	CAR_DETECT_POSITIVE_SAMPLES = MAX_VEHICLE_SAMPLES-2, //
+	MAX_VEHICLE_NO_UPDATE_FREQ = 15 // in frames
 };
 
 #define K_VARY_FACTOR 0.2f
@@ -78,7 +105,7 @@ void FindResponses(IplImage *img, int startX, int endX, int y, std::vector<int>&
 {
     // scans for single response: /^\_
 
-	const int row = y * img->width * img->depth/8;
+	const int row = y * img->width * img->nChannels;
 	unsigned char* ptr = (unsigned char*)img->imageData;
 
     int step = (endX < startX) ? -1: 1;
@@ -106,25 +133,251 @@ void FindResponses(IplImage *img, int startX, int endX, int y, std::vector<int>&
     }
 }
 
-void vehicleDetection(IplImage* half_frame, CvHaarClassifierCascade* cascade, CvMemStorage* haarStorage) {
+unsigned char pixel(IplImage* img, int x, int y) {
+	return (unsigned char)img->imageData[(y*img->width+x)*img->nChannels];
+}
 
-	// Haar Car detection
-	double scale = 1;
-	double scale_factor = 1.05; // every iteration increases scan window by 5%
-	int min_neighbours = 2; // minus 1, number of rectangles, that the object consists of
-	CvSeq* cars = cvHaarDetectObjects(half_frame, cascade, haarStorage, scale_factor, min_neighbours, CV_HAAR_DO_CANNY_PRUNING);
+int findSymmetryAxisX(IplImage* half_frame, CvPoint bmin, CvPoint bmax) {
+  
+  float value = 0;
+  int axisX = -1; // not found
+  
+  int xmin = bmin.x;
+  int ymin = bmin.y;
+  int xmax = bmax.x;
+  int ymax = bmax.y;
+  int half_width = half_frame->width/2;
+  int maxi = 1;
 
-	/* draw all the rectangles */
-	for (int i = 0; i < cars->total; i++) {
-		CvRect rc = *(CvRect*)cvGetSeqElem(cars, i);
-		cvRectangle(half_frame, 
-					cvPoint(rc.x*scale, rc.y*scale),
-						cvPoint((rc.x+rc.width)*scale, (rc.y+rc.height)*scale),
-						CV_RGB(0,255,0), 2);
+  for(int x=xmin, j=0; x<xmax; x++, j++) {
+	float HS = 0;
+    for(int y=ymin; y<ymax; y++) {
+		int row = y*half_frame->width*half_frame->nChannels;
+        for(int step=1; step<half_width; step++) {
+          int neg = x-step;
+          int pos = x+step;
+		  unsigned char Gneg = (neg < xmin) ? 0 : (unsigned char)half_frame->imageData[row+neg*half_frame->nChannels];
+          unsigned char Gpos = (pos >= xmax) ? 0 : (unsigned char)half_frame->imageData[row+pos*half_frame->nChannels];
+          HS += abs(Gneg-Gpos);
+        }
+    }
+
+	if (axisX == -1 || value > HS) { // find minimum
+		axisX = x;
+		value = HS;
+	}
+  }
+
+  return axisX;
+}
+
+bool hasVertResponse(IplImage* edges, int x, int y, int ymin, int ymax) {
+	bool has = (pixel(edges, x, y) > BW_TRESHOLD);
+	if (y-1 >= ymin) has &= (pixel(edges, x, y-1) < BW_TRESHOLD);
+	if (y+1 < ymax) has &= (pixel(edges, x, y+1) < BW_TRESHOLD);
+	return has;
+}
+
+int horizLine(IplImage* edges, int x, int y, CvPoint bmin, CvPoint bmax, int maxHorzGap) {
+
+	// scan to right
+	int right = 0;
+	int gap = maxHorzGap;
+	for (int xx=x; xx<bmax.x; xx++) {
+		if (hasVertResponse(edges, xx, y, bmin.y, bmax.y)) {
+			right++;
+			gap = maxHorzGap; // reset
+		} else {
+			gap--;
+			if (gap <= 0) {
+				break;
+			}
+		}
 	}
 
-	cvShowImage("Half-frame", half_frame);
-	cvMoveWindow("Half-frame", half_frame->width*2+10, 0); 
+	int left = 0;
+	gap = maxHorzGap;
+	for (int xx=x-1; xx>=bmin.x; xx--) {
+		if (hasVertResponse(edges, xx, y, bmin.y, bmax.y)) {
+			left++;
+			gap = maxHorzGap; // reset
+		} else {
+			gap--;
+			if (gap <= 0) {
+				break;
+			}
+		}
+	}
+
+	return left+right;
+}
+
+bool vehicleValid(IplImage* half_frame, IplImage* edges, Vehicle* v, int& index) {
+
+	index = -1;
+
+	// first step: find horizontal symmetry axis
+	v->symmetryX = findSymmetryAxisX(half_frame, v->bmin, v->bmax);
+	if (v->symmetryX == -1) return false;
+
+	// second step: cars tend to have a lot of horizontal lines
+	int hlines = 0;
+	for (int y = v->bmin.y; y < v->bmax.y; y++) {		
+		if (horizLine(edges, v->symmetryX, y, v->bmin, v->bmax, 2) > CAR_H_LINE_LENGTH) {
+#if _DEBUG
+			cvCircle(half_frame, cvPoint(v->symmetryX, y), 2, PURPLE);
+#endif
+			hlines++;
+		}
+	}
+
+	int midy = (v->bmax.y + v->bmin.y)/2;
+
+	// third step: check with previous detected samples if car already exists
+	int numClose = 0;
+	float closestDist = 0;
+	for (int i = 0; i < samples.size(); i++) {
+		int dx = samples[i].center.x - v->symmetryX;
+		int dy = samples[i].center.y - midy;
+		float Rsqr = dx*dx + dy*dy;
+		
+		if (Rsqr <= samples[i].radi*samples[i].radi) {
+			numClose++;
+			if (index == -1 || Rsqr < closestDist) {
+				index = samples[i].vehicleIndex;
+				closestDist = Rsqr;
+			}
+		}
+	}
+
+	return (hlines >= CAR_DETECT_LINES || numClose >= CAR_DETECT_POSITIVE_SAMPLES);
+}
+
+void removeOldVehicleSamples(unsigned int currentFrame) {
+	// statistical sampling - clear very old samples
+	std::vector<VehicleSample> sampl;
+	for (int i = 0; i < samples.size(); i++) {
+		if (currentFrame - samples[i].frameDetected < MAX_VEHICLE_SAMPLES) {
+			sampl.push_back(samples[i]);
+		}
+	}
+	samples = sampl;
+}
+
+void removeSamplesByIndex(int index) {
+	// statistical sampling - clear very old samples
+	std::vector<VehicleSample> sampl;
+	for (int i = 0; i < samples.size(); i++) {
+		if (samples[i].vehicleIndex != index) {
+			sampl.push_back(samples[i]);
+		}
+	}
+	samples = sampl;
+}
+
+void removeLostVehicles(unsigned int currentFrame) {
+	// remove old unknown/false vehicles & their samples, if any
+	for (int i=0; i<vehicles.size(); i++) {
+		if (vehicles[i].valid && currentFrame - vehicles[i].lastUpdate >= MAX_VEHICLE_NO_UPDATE_FREQ) {
+			printf("\tremoving inactive car, index = %d\n", i);
+			removeSamplesByIndex(i);
+			vehicles[i].valid = false;
+		}
+	}
+}
+
+void vehicleDetection(IplImage* half_frame, CvHaarClassifierCascade* cascade, CvMemStorage* haarStorage) {
+
+	static unsigned int frame = 0;
+	frame++;
+	printf("*** vehicle detector frame: %d ***\n", frame);
+
+	removeOldVehicleSamples(frame);
+
+	// Haar Car detection
+	const double scale_factor = 1.05; // every iteration increases scan window by 5%
+	const int min_neighbours = 2; // minus 1, number of rectangles, that the object consists of
+	CvSeq* rects = cvHaarDetectObjects(half_frame, cascade, haarStorage, scale_factor, min_neighbours, CV_HAAR_DO_CANNY_PRUNING);
+
+	// Canny edge detection of the minimized frame
+	if (rects->total > 0) {
+		printf("\thaar detected %d car hypotheses\n", rects->total);
+		IplImage *edges = cvCreateImage(cvSize(half_frame->width, half_frame->height), IPL_DEPTH_8U, 1);
+		cvCanny(half_frame, edges, CANNY_MIN_TRESHOLD, CANNY_MAX_TRESHOLD);
+
+		/* validate vehicles */
+		for (int i = 0; i < rects->total; i++) {
+			CvRect* rc = (CvRect*)cvGetSeqElem(rects, i);
+			
+			Vehicle v;
+			v.bmin = cvPoint(rc->x, rc->y);
+			v.bmax = cvPoint(rc->x + rc->width, rc->y + rc->height);
+			v.valid = true;
+
+			int index;
+			if (vehicleValid(half_frame, edges, &v, index)) { // put a sample on that position
+				
+				if (index == -1) { // new car detected
+
+					v.lastUpdate = frame;
+
+					// re-use already created but inactive vehicles
+					for(int j=0; j<vehicles.size(); j++) {
+						if (vehicles[j].valid == false) {
+							index = j;
+							break;
+						}
+					}
+					if (index == -1) { // all space used
+						index = vehicles.size();
+						vehicles.push_back(v);
+					}
+					printf("\tnew car detected, index = %d\n", index);
+				} else {
+					// update the position from new data
+					vehicles[index] = v;
+					vehicles[index].lastUpdate = frame;
+					printf("\tcar updated, index = %d\n", index);
+				}
+
+				VehicleSample vs;
+				vs.frameDetected = frame;
+				vs.vehicleIndex = index;
+				vs.radi = (MAX(rc->width, rc->height))/4; // radius twice smaller - prevent false positives
+				vs.center = cvPoint((v.bmin.x+v.bmax.x)/2, (v.bmin.y+v.bmax.y)/2);
+				samples.push_back(vs);
+			}
+		}
+
+		cvShowImage("Half-frame[edges]", edges);
+		cvMoveWindow("Half-frame[edges]", half_frame->width*2+10, half_frame->height); 
+		cvReleaseImage(&edges);
+	} else {
+		printf("\tno vehicles detected in current frame!\n");
+	}
+
+	removeLostVehicles(frame);
+
+	printf("\ttotal vehicles on screen: %d\n", vehicles.size());
+}
+
+void drawVehicles(IplImage* half_frame) {
+
+	// show vehicles
+	for (int i = 0; i < vehicles.size(); i++) {
+		Vehicle* v = &vehicles[i];
+		if (v->valid) {
+			cvRectangle(half_frame, v->bmin, v->bmax, GREEN, 1);
+			
+			int midY = (v->bmin.y + v->bmax.y) / 2;
+			cvLine(half_frame, cvPoint(v->symmetryX, midY-10), cvPoint(v->symmetryX, midY+10), PURPLE);
+		}
+	}
+
+	// show vehicle position sampling
+	/*for (int i = 0; i < samples.size(); i++) {
+		cvCircle(half_frame, cvPoint(samples[i].center.x, samples[i].center.y), samples[i].radi, RED);
+	}*/
 }
 
 void processSide(std::vector<Lane> lanes, IplImage *edges, bool right) {
@@ -147,20 +400,23 @@ void processSide(std::vector<Lane> lanes, IplImage *edges, bool right) {
 		std::vector<int> rsp;
 		FindResponses(edges, midx, ENDX, y, rsp);
 
-		for (int i=0; i<rsp.size(); i++) {
+		/*for (int i=0; i<rsp.size(); i++) {
 			CvPoint p = cvPoint(rsp[i], y);
 			bool found = false;
-			
+		}	*/
+
+		if (rsp.size() > 0) {
+			CvPoint p = cvPoint(rsp[0], y);
+
 			for (int j=0; j<lanes.size(); j++) {
 				if (!lanes[j].box.contains(p)) continue;
-
 				float d = dist2line(cvPoint2D32f(lanes[j].p0.x, lanes[j].p0.y), 
 						cvPoint2D32f(lanes[j].p1.x, lanes[j].p1.y), 
 						cvPoint2D32f(p.x, p.y));
 
 				if (d <= MAX_RESPONSE_DIST) {
 					//right[j].votes++;
-					found = true;
+					//found = true;
 					if (first_lane == -1) {
 						first_lane = j;
 					}
@@ -172,7 +428,7 @@ void processSide(std::vector<Lane> lanes, IplImage *edges, bool right) {
 			if (found) {
 				cvCircle(temp_frame, p, 2, CV_RGB(0,255,0), 2);
 			}*/
-		}	
+		}
 	}
 
 	if (first_lane != -1) {
@@ -232,14 +488,27 @@ void processLanes(CvSeq* lines, IplImage* edges, IplImage* temp_frame) {
 		if (fabs(angle) <= LINE_REJECT_DEGREES) { // reject near horizontal lines
 			continue;
 		}
-		
+
+		// assume that vanishing point is close to the image horizontal center
+		//y = kx + b;
+		float k = dy/(float)dx;
+		float b = line[0].y - k*line[0].x;
+		float x  = -b/k;
+
+		const float w2 = temp_frame->width/2;
+		//TODO: does not work always (e.g. when the car is changing lanes!!! HT lines get lost!)
+		if (fabs(w2 - x) >= 50) {
+			continue;
+		}
+
+		// assign lane's side based by its midpoint position 
 		int midx = (line[0].x + line[1].x) / 2;
 		CvScalar s;
 			
-		if (midx < temp_frame->width/2 && angle < 0) {
+		if (midx < temp_frame->width/2) {
 			s = CV_RGB(255,0,0);
 			left.push_back(Lane(line[0], line[1], angle));
-		} else if (midx > temp_frame->width/2 && angle > 0) {
+		} else if (midx > temp_frame->width/2) {
 			s = CV_RGB(0,0,255);
 			right.push_back(Lane(line[0], line[1], angle));
 		}
@@ -290,9 +559,6 @@ int main(void)
 	video_size.height = (int) cvGetCaptureProperty(input_video, CV_CAP_PROP_FRAME_HEIGHT);
 	video_size.width = (int) cvGetCaptureProperty(input_video, CV_CAP_PROP_FRAME_WIDTH);
 
-	/// Create a Trackbar for user to enter threshold
-	//createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
-	
 	long current_frame = 0;
 	int key_pressed = 0;
 	IplImage *frame = NULL;
@@ -334,7 +600,12 @@ int main(void)
 			rho, theta, HOUGH_TRESHOLD, HOUGH_MIN_LINE_LENGTH, HOUGH_MAX_LINE_GAP);
 
 		processLanes(lines, edges, temp_frame);
+		
+		// process vehicles
 		vehicleDetection(half_frame, cascade, haarStorage);
+		drawVehicles(half_frame);
+		cvShowImage("Half-frame", half_frame);
+		cvMoveWindow("Half-frame", half_frame->width*2+10, 0); 
 
 		// show middle line
 		cvLine(temp_frame, cvPoint(frame_size.width/2,0), 
